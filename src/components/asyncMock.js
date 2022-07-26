893ed3707eb59df4e5720e3b87d0e144a863cf0c
Marilina Roman <roman.marilinaf@gmail.com>
2022-07-26 19:55:25 -0300

    version 0.8: change data

--- src/components/asyncMock.js
+++ src/components/asyncMock.js
@@ -4,31 +4,12 @@ const products = [
         name: 'Plate One',
         category: 'Plate',
         price:7.5,
+        description: 'Ceramic plate ideal for day to day, or your special moments, do not forget to pass this tableware, ideal for your kitchen, dining table.',
         stock: 0,
         color: 'cream',
-        description: 'Ceramic plate ideal for day to day, or your special moments, do not forget to pass this tableware, ideal for your kitchen, dining table.',
         img: 'images/plate/onecream.jpg'
-    },
-    {
+    },{
         id:2,
-        name: 'Plate One',
-        category: 'Plate',
-        price:7.5,
-        stock: 24,
-        color: 'green',
-        description: 'Ceramic plate ideal for day to day, or your special moments, do not forget to pass this tableware, ideal for your kitchen, dining table.',
-        img: 'images/plate/onegreen.jpg'
-    },{
-        id:3,
-        name: 'Plate One',
-        category: 'Plate',
-        price:7.5,
-        stock: 6,
-        color: 'white',
-        description: 'Ceramic plate ideal for day to day, or your special moments, do not forget to pass this tableware, ideal for your kitchen, dining table.',
-        img: 'images/plate/onewhite.jpg'
-    },{
-        id:4,
         name: 'Plate Two',
         category: 'Plate',
         price:9,
@@ -37,25 +18,7 @@ const products = [
         description: 'Ideal dish for your table, give a special touch to your family meals, with guests, even to your cupboards or showcases in the living room.',
         img: 'images/plate/twocream.jpg'
     },{
-        id:5,
-        name: 'Plate Two',
-        category: 'Plate',
-        price:9,
-        stock: 144,
-        color: 'green',
-        description: 'Ideal dish for your table, give a special touch to your family meals, with guests, even to your cupboards or showcases in the living room.',
-        img: 'images/plate/twogreen.jpg'
-    },{
-        id:6,
-        name: 'Plate Three',
-        category: 'Plate',
-        price:10,
-        stock: 72,
-        color: 'white',
-        description: 'Ceramic dinner plate in the shape of a flower, ideal for special moments, or simply to make your day to day special. Give your kitchen and your table a different touch. Dimensions diameter 16.5 cm height 2.2 cm. Microwave, oven, dishwasher and refrigerator safe',
-        img: 'images/plate/threewhite.jpg'
-    },{
-        id:7,
+        id:3,
         name: 'Plate Three',
         category: 'Plate',
         price:10,
@@ -63,21 +26,51 @@ const products = [
         color: 'green',
         description: 'Ceramic dinner plate in the shape of a flower, ideal for special moments, or simply to make your day to day special. Give your kitchen and your table a different touch. Dimensions diameter 16.5 cm height 2.2 cm. Microwave, oven, dishwasher and refrigerator safe',
         img: 'images/plate/threegreen.jpg'
-    },{
-        id:8,
-        name: 'Plate Three',
+    }
+]
+
+const subProducts = [
+    {
+        idProduct: 1, 
         category: 'Plate',
-        price:10,
+        idSub:1,
+        img: 'images/plate/onegreen.jpg',
+        color: 'green',
+        stock:12,
+    },{
+        idProduct: 1, 
+        category: 'Plate',
+        idSub:2,
+        color: 'white',
+        img: 'images/plate/onewhite.jpg',
+        stock:24,
+    },{
+        idProduct:2,
+        category: 'Plate',
+        idSub:1,
+        stock: 144,
+        color: 'green',
+        img: 'images/plate/twogreen.jpg'
+    },{
+        idProduct:3,
+        idSub:1,
+        category: 'Plate',
+        stock: 72,
+        color: 'white',
+        img: 'images/plate/threewhite.jpg'
+    },{
+        idProduct:3,
+        category: 'Plate',
+        idSub:2,
         stock: 72,
         color: 'cream',
-        description: 'Ceramic dinner plate in the shape of a flower, ideal for special moments, or simply to make your day to day special. Give your kitchen and your table a different touch. Dimensions diameter 16.5 cm height 2.2 cm. Microwave, oven, dishwasher and refrigerator safe',
         img: 'images/plate/threecream.jpg'
     }
 ]
 export const getProducts = () =>{
     return new Promise((resolve) =>{
         setTimeout(() =>{
-            resolve(products)
+            resolve(products, subProducts)
         }  , 2000)
     })
 }
