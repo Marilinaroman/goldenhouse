643cf74cc4b3077dbdb0abbc67f345998db699bc
Marilina Roman <roman.marilinaf@gmail.com>
2022-08-25 19:24:57 -0300

    version 1.6: add segurity to firebase

--- src/service/firebase/index.js
+++ src/service/firebase/index.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 
-// Your web app's Firebase configuration
+// Your web app's Fprocess
 const firebaseConfig = {
-    apiKey: "AIzaSyA-umPXil3Ntv8rVDYTjI2lNNuVgmpL8LA",
-    authDomain: "bazar-golden-house.firebaseapp.com",
-    projectId: "bazar-golden-house",
-    storageBucket: "bazar-golden-house.appspot.com",
-    messagingSenderId: "257127563242",
-    appId: "1:257127563242:web:a2b69f3a2c747133d8bc93"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
